dc12ded94759c5eb5b35c53ffc978a1b372f9675
k.manin <mk13kos@gmail.com>
2015-10-01 11:30:37 +0600

    Fix views

--- views/default/show.php
+++ views/default/show.php
@@ -1,10 +1,15 @@
-<?php use yii\helpers\Html; ?>
+<?php
+use yii\helpers\Html;
+use yii\helpers\Url;
+
+$url = substr(Url::current(), 0, strrpos(Url::current(), '/'));
+?>
 
 <div class="article-single">
 	<h1 class="article-title"><?= $model->title ?></h1>
 
     <div class="article-text block"><?= $model->text ?></div>
 
-	<div class="article-back"><?= Html::a('&larr; Все статьи','/articles', ['class'=>'link']);?></div>
+	<div class="article-back"><?= Html::a('&larr; Все статьи', $url, ['class' => 'link']);?></div>
 
 </div>
